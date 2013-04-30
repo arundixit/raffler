192c6e7507ba296f9e85034be5b11e5aed2fb4b0
arundixit <arun.dixit@haatopia.in>
2013-04-30 23:45:42 +0530

    raffler project
    sir please check the coffee script and index.html file  add entry
    fuction is not working

---
+++ raffler/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raffler::Application.config.secret_token = 'b3dc206f6be4b2536533fa29542e3ae5ae6dd00b75bb6211897928814384a172c6f6ed52a74b128552234dd0c4fc57db25c343e0aa4a10ef6c95b8e1aea2ce76'
